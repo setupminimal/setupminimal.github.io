fc55a232197b9c14990760bfb204f35ee72989ae
Daroc Alden <setupminimal@gmail.com>
2021-07-04 11:10:16 -0400

    Publishing first episode of LMAL

---
+++ src/blog/2021-07-04-Lets-Make-a-Language-Episode-1.poly.pm
@@ -0,0 +1,7 @@
+#lang pollen
+
+◊title{Let's Make a Language: Episode 1}
+
+Well, the first episode of Let's Make a Language has happened. You can watch it here: ◊link[#:to "https://youtu.be/DzBUZ8ccQm4"]{https://youtu.be/DzBUZ8ccQm4}.
+
+And you can also join us on the 17th of July at 11:00 AM for the next livestream at ◊link[#:to "https://twitch.tv/setupminimal"]{https://twitch.tv/setupminimal}.
